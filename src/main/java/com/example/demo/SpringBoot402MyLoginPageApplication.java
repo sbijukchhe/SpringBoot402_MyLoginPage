ea47239208a21f4fb3e958fc909b2b53303158a6
sbijukchhe <skbijukchhe@gmail.com>
2019-08-21 14:56:35 -0400

    Adding Custom Login Page

---
+++ src/main/java/com/example/demo/SpringBoot402MyLoginPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot402MyLoginPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot402MyLoginPageApplication.class, args);
+    }
+
+}
